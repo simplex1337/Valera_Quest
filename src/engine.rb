7bead4fb321e24c9225ce5a5cbecb2751b526828
Григорий Тарасенко <fle4er@yandex.ru>
2018-10-17 11:01:47 +0700

    Отрефакторил движок

--- src/engine.rb
+++ src/engine.rb
@@ -1,49 +1,47 @@
 require 'yaml'
-require 'Event.rb'
-require 'Valera.rb'
+require 'event.rb'
+require 'valera.rb'
 
-class Engine
+module ValeraQuest
+  class Engine
     attr_reader :events, :valerra
 
     def initialize
-        puts 'Loading events from config...'
-        load_events '../conf/conf.yml'
-        puts 'Born one more freak Valera...'
-        @valerra = Valera.new
+      load_events '../conf/conf.yml'
+      @valerra = Valera.new
     end
 
     def load_events(file)
-        events_from_file = YAML.load_file file
-        @events Array.new
-        events_from_file.each do |line|
-            event = Event.new line
-            @events.push event
-        end
+      events_from_file = YAML.load_file file
+      @events Array.new
+      events_from_file.each do |line|
+        event = Event.new line
+        @events.push event
+      end
 
     end
 
     def event_list
-        list = Array.new
-        self.events.each do |event|
-            list.push event.description
-        end
-        list
+      list = Array.new
+      self.events.each do |event|
+        list.push event.description
+      end
+      list
     end
 
     def valeras_doom(number)
-        if number >= events.size then
-            raise 'We dont have ascing event'
-        end
+      if number >= events.size then
+        raise 'We dont have ascing event'
+      end
 
-        self.events.at(number).apply(@valerra)
+      self.events.at(number).apply(@valerra)
     end
 
     def save_valera_state
     end
 
     def load_valera_state
-        valera_state = YAML.load_file '../valera.yml'
+      valera_state = YAML.load_file '../valera.yml'
     end
+  end
 end
-
-Engine.new

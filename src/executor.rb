9805cb231ae2a66920f5db1ea2371b1c7fc147ed
betman1488 <bedaal123@gmail.com>
2018-10-17 10:44:46 +0700

    Меню игры

---
+++ src/executor.rb
@@ -0,0 +1,51 @@
+class Executor 
+	attr_reader :engine 
+	
+	def menu
+		puts '1 - New Game'
+		puts '2 - Load Game'
+		puts '3 - Exit'
+		
+		rez_menu = gets.chomp.to_i
+		 
+		case rez_menu
+			when 1
+				new_game	
+			
+			when 2
+				load_game
+			
+			when 3
+				exit_game
+		end
+			
+	end
+	
+	def new_game
+		puts 'New Game'
+		@engine = Engine.new
+		engine.initialize
+	end
+	
+	def load_game
+		@engine = Engine.new
+		engine.load_valera_state
+	end
+	
+	def exit_game
+		puts 'Save Game?'
+		puts '1 - Yes | 2 - No'
+		exit_rez = gets.chomp.to_i
+				
+		case exit_rez
+			when 1
+				@engine = Engine.new
+				engine.save_valera_state
+				
+			when 2
+				exit 'God bay!'
+		end
+			
+	end
+
+end

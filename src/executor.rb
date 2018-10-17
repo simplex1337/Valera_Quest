494e79bc1e6523427983d2d2dc0f6065a513268f
betman1488 <bedaal123@gmail.com>
2018-10-17 20:32:42 +0700

    В роботе

--- src/executor.rb
+++ src/executor.rb
@@ -1,6 +1,6 @@
 module ValeraQuest
   class Executor 
-    attr_reader :engine 
+    attr_reader :engine, :valera
 	
     def initialize
       @engine = Engine.new
@@ -9,7 +9,8 @@ module ValeraQuest
     def menu
       puts '1 - New Game'
       puts '2 - Load Game'
-      puts '3 - Exit'
+      puts '3 - Save Game'
+      puts '4 - Exit'
       rez_menu = gets.chomp.to_i
 		
       case rez_menu
@@ -18,6 +19,8 @@ module ValeraQuest
         when 2
           load_game
         when 3
+		  save_game
+        when 4
           exit_game
         else
           puts 'Input error!'
@@ -40,11 +43,16 @@ module ValeraQuest
     end
 	
     def new_game
-      puts 'New Game'
+      game
     end
 	
     def load_game
       self.engine.load_valera_state
+      game
+    end
+    
+    def save_game
+      self.save_valera_state
     end
 	
     def exit_game
@@ -53,12 +61,31 @@ module ValeraQuest
       exit_rez = gets.chomp.to_i
 				
       case exit_rez
-        when 1
+        when 1 
           self.engine.save_valera_state
         when 2
           exit 'Good bay!'
         else
           puts 'Input error!'
+      end
+    end
+    
+    def game
+      puts self.valera.get_full_stat
+      puts 'Select event:'
+      puts '0 - ' event_list.list(0)
+      puts '1 - ' event_list.list(1)
+      puts '2 - ' event_list.list(2)
+      puts '3 - ' event_list.list(3)
+      puts '4 - ' event_list.list(4)
+      puts '5 - ' event_list.list(5)
+      event = gets.chomp.to_i      
+      
+      case 
+        when 0..5
+          engine_doom event
+        else
+          puts 'Input error event!'
       end
     end
   end
